d82e7e208ec480057b7c9026b325efa0f0d6b5d8
tajayebi <tajayebi@liveops.com>
2015-09-06 22:48:45 -0700

    Create fa.i18n.js
    Persian (Farsi) Translation

---
+++ i18n/fa.i18n.js
@@ -0,0 +1,96 @@
+
+i18n.map("fa", {
+	resetPasswordDialog: {
+		title: "رمز عبور خود را تنظیم مجدد",
+		newPassword: "کلمه عبور جدید",
+		newPasswordAgain: "رمز عبور (دوباره)",
+		cancel: "لغو",
+		submit: "تنظیم رمز عبور"
+	},
+	enrollAccountDialog: {
+		title: "انتخاب یک رمز عبور",
+		newPassword: "کلمه عبور جدید",
+		newPasswordAgain: "رمز عبور (دوباره)",
+		cancel: "نزدیک",
+		submit: "تنظیم رمز عبور"
+	},
+	justVerifiedEmailDialog: {
+		verified: "آدرس ایمیل تایید",
+		dismiss: "پنهان کن"
+	},
+	loginButtonsMessagesDialog: {
+		dismiss: "پنهان کن",
+	},
+	loginButtonsLoggedInDropdownActions: {
+		password: "تغییر رمز عبور",
+		signOut: "خروج"
+	},
+	loginButtonsLoggedOutDropdown: {
+		signIn: "ثبت نام در",
+		up: "اضافه کردن"
+	},
+	loginButtonsLoggedOutPasswordServiceSeparator: {
+		or: "یا"
+	},
+	loginButtonsLoggedOutPasswordService: {
+		create: "درست",
+		signIn: "ثبت نام در",
+		forgot: "رمز عبور را فراموش کرده اید؟",
+		createAcc: "ایجاد حساب کاربری"
+	},
+	forgotPasswordForm: {
+		email: "ایمیل",
+		reset: "تنظیم مجدد رمز ورود",
+		invalidEmail: "ایمیل نامعتبر است"
+	},
+	loginButtonsBackToLoginLink: {
+		back: "لغو"
+	},
+	loginButtonsChangePassword: {
+		submit: "تغییر رمز عبور",
+		cancel: "لغو"
+	},
+	loginButtonsLoggedOutSingleLoginButton: {
+		signInWith: "ورود به سیستم با",
+		configure: "پیکربندی",
+	},
+	loginButtonsLoggedInSingleLogoutButton: {
+		signOut: "خروج"
+	},
+	loginButtonsLoggedOut: {
+		noLoginServices: "ورود بدون خدمات پیکربندی"
+	},
+	loginFields: {
+		usernameOrEmail: "نام کاربری یا پست",
+		username: "نام کاربری",
+		email: "ایمیل",
+		password: "رمز عبور"
+	},
+	signupFields: {
+		username: "نام کاربری",
+		email: "ایمیل",
+		emailOpt: "پست الکترونیک (اختیاری)",
+		password: "رمز عبور",
+		passwordAgain: "رمز عبور (دوباره)"
+	},
+	changePasswordFields: {
+		currentPassword: "رمز عبور فعلی",
+		newPassword: "رمز عبور جدید",
+		newPasswordAgain: "رمز عبور جدید"
+	},
+	infoMessages : {
+		emailSent: "ایمیل ارسال",
+		passwordChanged: "تغییر رمز عبور"
+	},
+	errorMessages: {
+		genericTitle: "خطا وجود دارد",
+		userNotFound: "کاربر یافت نشد",
+		invalidEmail: "ایمیل نامعتبر است",
+		incorrectPassword: "رمز عبور اشتباه",
+		usernameTooShort: "نام کاربری حداقل باید ۳ کاراکتر باشد",
+		passwordTooShort: "رمز عبور باید حداقل ۶ کاراکتر باشد",
+		passwordsDontMatch: "کلمه عبور هماهنگ نیست",
+		newPasswordSameAsOld: "کلمه عبور جدید و قدیمی باید متفاوت باشد",
+		signupsForbidden: "ثبت نام ممنوع"
+	}
+});
